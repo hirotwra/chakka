2d3b02a57f8c7a8f825095a3712c92938aeaf12f
HiroyasuTawara <45650777+HiroyasuTawara@users.noreply.github.com>
2022-12-09 11:20:13 +0900

    Deviseログイン機能実装完了

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ProjectsController < ApplicationController
+  before_action :authenticate_user!
   def index
     projects= Project.order(updated_at: :desc)
     render json: projects
